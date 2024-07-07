d6ad068e88399e5045a89fe3662dd7623283b107
Kaisup <147511087+Kaisup@users.noreply.github.com>
2024-07-07 21:29:50 +0700

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,12 +10,14 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+//Dog game
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
-  
+
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
