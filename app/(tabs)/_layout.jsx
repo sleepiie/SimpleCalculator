2cbdf16c0fca6f052fa830cf7940033bae323d99
sleepiie <122366817+sleepiie@users.noreply.github.com>
2024-07-08 19:23:17 +0700

    update ui
    some update

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -3,16 +3,14 @@ import { Text, View, SafeAreaView } from 'react-native';
 import Row from '@/components/row';
 import Button from '@/components/Button';
 import styles from './src/style';
-import SButton from '@/components/sButton';
+
 
 
 export default function App() {
-
     return (
-   
       <View style={styles.container}>
-        <SafeAreaView style={styles.safeArea}>
-          <Button text="7" />
+        <SafeAreaView>
+          <Text style={styles.value}>0</Text>
           <Row>
             <Button text="AC" theme="accent" />
             <Button text="<-" theme="accent" />
--- app/(tabs)/src/style.jsx
+++ app/(tabs)/src/style.jsx
@@ -6,16 +6,22 @@ const styles = StyleSheet.create({
 
     container: {
         flex: 1,
+        backgroundColor: 'black',
         justifyContent: 'flex-end',
+        
     },
     text: {
         color: '#fff',
         fontSize: 24,
     },
-    safeArea: {
-        flex: 1,
-        backgroundColor:"black",
+    value: {
+        color: '#fff',
+        fontSize: 48,
+        textAlign: 'right',
+        marginRight: 20,
+        marginBottom: 10,
     },
+
 });
 
 export default styles;
