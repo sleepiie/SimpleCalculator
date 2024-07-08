9a200e0fe575441241921d87bdefe740cf387ff7
sleepiie <122366817+sleepiie@users.noreply.github.com>
2024-07-08 15:49:11 +0700

    ui update
    update ui and style

---
+++ app/(tabs)/src/style.jsx
@@ -0,0 +1,21 @@
+import { StyleSheet, } from "react-native";
+import { Dimensions } from "react-native";
+
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    text: {
+        color: '#fff',
+        fontSize: 24,
+    },
+    safeArea: {
+        flex: 1,
+        backgroundColor:"black",
+    },
+});
+
+export default styles;
