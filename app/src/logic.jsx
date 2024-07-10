578f8b9cd6b255943fee003e7300fb9cf1c9bea5
lemuruz <nakabodeepatiparn@gmail.com>
2024-07-10 23:33:44 +0700

    fix +-*/ show 0.0e

--- app/src/logic.jsx
+++ app/src/logic.jsx
@@ -59,16 +59,28 @@ const handleEqual = (state) => {
 
   switch (operator) {
     case "+":
+      if (current == "0") return{
+        currentValue : "0"
+
+      }
       return {
         currentValue: formatValue(previous + current),
         ...resetState,
       };
     case "-":
+      if (current == "0") return{
+        currentValue : "0"
+
+      }
       return {
         currentValue: formatValue(previous - current),
         ...resetState,
       };
     case "*":
+      if (current == "0") return{
+        currentValue : "0"
+
+      }
       return {
         currentValue: formatValue(previous * current),
         ...resetState,
